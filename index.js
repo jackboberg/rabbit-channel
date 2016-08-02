fb88d410c5e565eb111c6713c9eda3413041b59d
Jack Boberg <jack.boberg@gmail.com>
2016-08-02 14:37:39 -0400

    Import channel function from @modulus/rabbit-rpc

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function () {
-  // noop
+const Amqp = require('amqplib/callback_api');
+
+module.exports = function Channel(url, opts, done) {
+  Amqp.connect(url, opts, function (err, conn) {
+    if (err) return done(err);
+
+    conn.createChannel(function (err, ch) {
+      if (err) return done(err);
+
+      done(null, ch, conn);
+    });
+  });
 };
