ea5cf502732a5e602b12c1e5ece2ca85628b9cc5
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2017-07-14 18:34:11 -0400

    refactor: run standard --fix
    Update `lint` script to use `standard` instead of `mod-standard`
    
    Clean up code to pass lint

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const Amqp = require('amqplib/callback_api');
+const Amqp = require('amqplib/callback_api')
 
-module.exports = function Channel(url, opts, done) {
+module.exports = function Channel (url, opts, done) {
   if (typeof url === 'function') {
-    done = url;
-    url = opts = null;
+    done = url
+    url = opts = null
   }
 
   if (typeof opts === 'function') {
-    done = opts;
-    opts = null;
+    done = opts
+    opts = null
   }
 
   Amqp.connect(url, opts, function (err, conn) {
-    if (err) return done(err);
+    if (err) return done(err)
 
     conn.createChannel(function (err, ch) {
-      if (err) return done(err);
+      if (err) return done(err)
 
-      done(null, ch, conn);
-    });
-  });
-};
+      done(null, ch, conn)
+    })
+  })
+}
