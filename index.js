9e2371502457c6092a5a1daffd250b98ae1d9c94
Jack Boberg <hello@jackboberg.info>
2017-07-28 13:46:15 -0400

    refactor: Reorder functions and use more arrows

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Amqp = require('amqplib/callback_api')
 
-const isFunction = (value) => typeof value === 'function'
-
 module.exports = (url, opts, done) => {
   if (isFunction(url)) [url, opts, done] = [null, {}, url]
   if (isFunction(opts)) [opts, done] = [{}, opts]
 
-  Amqp.connect(url, opts, function (err, conn) {
+  Amqp.connect(url, opts, (err, conn) => {
     if (err) return done(err)
 
     const closeConnection = (err) => {
@@ -14,9 +12,11 @@ module.exports = (url, opts, done) => {
       done(err)
     }
 
-    conn.createChannel(function (err, ch) {
+    conn.createChannel((err, ch) => {
       if (err) closeConnection(err)
       else done(null, ch, conn)
     })
   })
 }
+
+const isFunction = (value) => typeof value === 'function'
